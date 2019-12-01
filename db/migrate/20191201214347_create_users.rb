ce58eed9f0529c499111d4a25973cd40fcd7f1f1
Brad Alexander <brad.alexander913@gmail.com>
2019-12-01 21:44:40 +0000

    Automatically backed up by Learn

--- db/migrate/20191201214347_create_users.rb
+++ db/migrate/20191201214347_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t| 
+    create_table :users do |t|
       t.string :username
-      t.string :password 
-      t.integer :balance 
-    end 
+      t.string :password
+      t.integer :balance
+    end
   end
 end
