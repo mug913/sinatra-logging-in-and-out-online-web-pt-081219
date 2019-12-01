5c27163003ba2b668e87c6cee806f33a5110a63a
Brad Alexander <brad.alexander913@gmail.com>
2019-12-01 22:13:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if !!Helpers.is_logged_in?(session)
-      erb :error
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
     else
-    @user = Helpers.current_user(session)
-    erb :account
-  end
+      erb :error
+    end
   end
 
   get '/logout' do
