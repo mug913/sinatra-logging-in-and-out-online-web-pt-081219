ff26669222bf74c1e3f35b1863ec6c6860a09970
Brad Alexander <brad.alexander913@gmail.com>
2019-12-01 22:26:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20191201214347) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.integer "balance"
+    t.decimal "balance"
   end
 
 end
